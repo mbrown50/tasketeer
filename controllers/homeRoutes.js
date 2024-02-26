c3d0043cc702ca5adcfd1987733da0ed1d864453
M <melissabrown@Melissas-MacBook-Pro.local>
2024-02-26 14:36:46 -0600

    login and user tasks

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,54 @@
 const router = require('express').Router();
 const { Task } = require('../models');
 
+const app = require('express')();
+app.use('/', (req, res) => {
+    var cookie = getcookie(req);
+   // console.log(cookie);
+});
+
+function getcookie(req) {
+    var cookie = req.headers.cookie;
+    // user=someone; session=mySessionID
+    return cookie.split(';');
+}
+
+function getCookieValue(req,key) {
+    //var cookies = document.cookie.split('; ');
+    var cookies = req.headers.cookie.split(';');
+    //console.log(cookies);
+    //console.log(key);
+    for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
+        //console.log(parts.shift().trim(), " ", key.trim());
+        var partName = parts.shift().trim();
+        var partValue = parts.shift().trim();
+        //console.log(partName); // value is here!!
+        //console.log(key)
+        if (partName == key.trim()) {
+        //if (parts.shift().trim() === key.trim()) {
+        //console.log("match");
+        //return decode(parts.join('='));
+        return partValue;
+      }
+    }
+    return null;
+  }
+  
+  function decode(s) {
+    return decodeURIComponent(s.replace(/\+/g, ' '));
+  }
+
 router.get('/', async (req, res) => {
     res.render('login');
 });
 
 router.get('/task', async (req, res) => {
-    let userID = 1;
+   // var userCookie = getcookie(res);
+    var userValue = getCookieValue(req, "user_id");
+    //console.log(userValue);
+
+    let userID = userValue;
+    //let userID = userCookie.user_id;
     try {
         const db = await Task.findAll({
            where:{
